4cd8b3f48b4191f6511b25b573089c06e5f0b189
pheliperost <phelipe_rost@hotmail.com>
2022-01-02 02:30:21 -0300

    Slider funcionando

--- App.js
+++ App.js
@@ -22,11 +22,10 @@ import {
   Stack,
   Box,
   Slider,
-  View
-  
+  View,
 } from 'native-base';
 import NativeBaseIcon from './src/components/NativeBaseIcon';
-import { render } from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod';
+import {render} from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod';
 
 // Color Switch Component
 function ToggleDarkMode() {
@@ -46,80 +45,65 @@ function ToggleDarkMode() {
   );
 }
 
-
-export const CheckB = () => {
-  const [groupValues, setGroupValues] = React.useState([])
+export const CheckB = props => {
+  const [groupValues, setGroupValues] = React.useState([]);
   return (
     <Checkbox.Group
       onChange={setGroupValues}
       value={groupValues}
       accessibilityLabel="choose numbers">
-      <Checkbox value="one" my={2}>
-        Fala
-      </Checkbox>
+      <Checkbox value="one">Fala</Checkbox>
       <Checkbox value="two">Leitura</Checkbox>
     </Checkbox.Group>
-  )
-  }
+  );
+};
 
-  export const Slid = () => {
-    const [onChangeValue, setOnChangeValue] = React.useState(70)
-    const [onChangeEndValue, setOnChangeEndValue] = React.useState(70)
-    return (
-      <Stack mx={5} space={4} alignItems="center" w="100%">
-        <Text>onChangeValue - {onChangeValue}</Text>
-        <Text>onChangeEndValue - {onChangeEndValue}</Text>
-  
-        <Box mx={5} w="250">
-          <Slider
-            defaultValue={70}
-            colorScheme="cyan"
-            onChange={(v) => {
-              setOnChangeValue(Math.floor(v))
-            }}
-            onChangeEnd={(v) => {
-              v && setOnChangeEndValue(Math.floor(v) && meuloopfunction)
-            }}
-          >
-            <Slider.Track>
-              <Slider.FilledTrack />
-            </Slider.Track>
-            <Slider.Thumb />
-          </Slider>
-        </Box>
-      </Stack>
-    )
+export const Slid = () => {
+  const [onChangeValue, setOnChangeValue] = React.useState(10);
+  const [onChangeEndValue, setOnChangeEndValue] = React.useState(10);
+  return (
+    <Stack mx={5} space={4} alignItems="center" w="100%">
+      <Text>onChangeValue - {onChangeValue}</Text>
+      <Text>onChangeEndValue - {onChangeEndValue}</Text>
+
+      <Box mx={5} w="250">
+        <Slider
+          defaultValue={3}
+          colorScheme="cyan"
+          onChange={v => {
+            setOnChangeValue(Math.floor(v));
+          }}
+          onChangeEnd={v => {
+            v && setOnChangeEndValue(Math.floor(v) && meuloopfunction(v));
+          }}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+      </Box>
+    </Stack>
+  );
+};
+
+const meuloopfunction = propsvalue => {
+  const myloop = [];
+  const numFinal = propsvalue;
+  for (let i = 0; i < numFinal; i++) {
+    myloop.push(<CheckB key={i} />);
   }
-  
-const meuloopfunction = () =>{
-    const myloop = [];
-    const numFinal = this.onChangeEndValue;
-    for(let i=1; i < numFinal; i++){
-      myloop.push(
-        <View key={i}>
-          <Text>onChangeValue </Text>
-        </View>
-      )
-    }
-    return myloop;
-  }
-  
-  export const Repeater = () =>{
-    return(
-        <Stack mx={5} space={4} alignItems="center" w="100%">
-           {meuloopfunction}
-        </Stack>
-    );
-  }
+  return myloop;
+};
+
+export const Repeater = () => {
+  return <Center>{meuloopfunction}</Center>;
+};
 const App = () => {
   return (
     <NativeBaseProvider>
       <HStack space={3} alignItems="flex-end">
         <ToggleDarkMode />
       </HStack>
-      <Box>
-        <Slid/>
-      </Box>
       <Center
         _dark={{bg: 'blueGray.900'}}
         _light={{bg: 'blueGray.50'}}
@@ -128,7 +112,7 @@ const App = () => {
         <VStack space={5} alignItems="center">
           <HStack space={2} alignItems="center">
             {Repeater}
-            <CheckB />
+            <Slid />
           </HStack>
         </VStack>
       </Center>
