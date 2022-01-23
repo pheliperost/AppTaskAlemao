2a052729c421eb9c0c41a3b23cf7473b926f16b1
pheliperost <phelipe_rost@hotmail.com>
2022-01-23 17:06:07 -0300

    remoçao darkmode e melhoria de código

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Link,
   Text,
@@ -47,44 +47,68 @@ function ToggleDarkMode() {
   );
 }
 
-
-
 const App = () => {
-  //const [checkB, setCheckB] = useState()
-  const [groupValues, setGroupValues] = React.useState([{serie: 'a', checkfala: 'a', checkleitura: 'as'}]);
+  const [groupValues, setGroupValues] = useState([
+    {serie: 'a', checkfala: 'a', checkleitura: 'as'}
+  ]);
+
+  const [checkboxItens, setCheckboxItens] = React.useState([
+   ]);
+  
+  /*  const [checkboxItens, setCheckboxItens] = React.useState([
+    {serie: 0, key: 0, index: 0},
+    {serie: 1, key: 1, index: 1},
+    {serie: 2, key: 2, index: 2},
+    {serie: 3, key: 3, index: 3},
+    {serie: 4, key: 4, index: 4},
+  ]); */
   const [onChangeValue, setOnChangeValue] = React.useState(30);
   const [onChangeEndValue, setOnChangeEndValue] = React.useState(30);
 
   const meuloopfunction = propsvalue => {
     const myloop = [];
     const numFinal = propsvalue;
-    for (let i = 0; i < numFinal; i++) {      
-      const _checkfala = [i ,"fala"].join();
-      const _checkleitura = [i ,"leitura"].join();
-      myloop.push({serie: i, checkfala: _checkfala, checkleitura: _checkleitura});
-      
+    for (let i = 0; i < numFinal; i++) {
+      myloop.push({
+        serie: i
+      }); 
+
     }
-    setGroupValues(myloop);
+    setCheckboxItens(myloop);
+  };
+
+
+  const CheckBoxDinamico = (item) => {
+    console.log("check di");
+    console.log(item.item.serie);
+    return (
+      <View>
+        <Text>Série {item.item.serie+1}</Text>
+        <Checkbox value="one" key={item.item+0.1}>
+          Fala
+        </Checkbox>
+        <Checkbox value="one" key={item.item+0.2}>
+          Leitura
+        </Checkbox>
+      </View>
+    );
   };
 
   useEffect(() => {
-    meuloopfunction(30);
-    console.log("groupValues");
-    console.log(groupValues);
-  }, []);
+    meuloopfunction(30)
+  },[]);
 
   return (
-    
     <NativeBaseProvider>
       <HStack space={3} alignItems="flex-end">
-        <ToggleDarkMode />
+        
       </HStack>
       <HStack space={1} alignItems="center">
-          <Stack mx={5} space={3} alignItems="baseline" w="85%">   
-            <Box w="100%" my="3"  alignItems="center">
-              <Text fontSize="xl">Defina a quantidade de séries</Text>           
-              <Text fontSize="xl">{onChangeValue} Itens</Text>           
-            </Box>   
+        <Stack mx={5} space={3} alignItems="baseline" w="85%">
+          <Box w="100%" my="3" alignItems="center">
+            <Text fontSize="xl">Defina a quantidade de séries</Text>
+            <Text fontSize="xl">{onChangeValue} Itens</Text>
+          </Box>
           <Slider
             defaultValue={30}
             minValue={0}
@@ -94,26 +118,27 @@ const App = () => {
               setOnChangeValue(Math.floor(v));
             }}
             onChangeEnd={v => {
-                setOnChangeEndValue(Math.floor(v));
+              setOnChangeEndValue(Math.floor(v));
             }}>
-              
-          <Slider.Track>
+            <Slider.Track>
               <Slider.FilledTrack />
-          </Slider.Track>
-          <Slider.Thumb />
+            </Slider.Track>
+            <Slider.Thumb />
           </Slider>
-            <Divider my="2" />          
+          <Divider my="2" />
         </Stack>
-        
       </HStack>
-      
+
       <Button small primary onPress={() => meuloopfunction(onChangeEndValue)}>
         <Text>Aplicar Filtro</Text>
       </Button>
-      <ScrollViewCheckBox valores={groupValues} />
-      <Button small primary onPress={() => setGroupValues([])}>
-        <Text>Limpar</Text>
-      </Button>
+      <ScrollView>
+      {checkboxItens.map(item => {
+        return (
+          <CheckBoxDinamico item={item} />
+        );
+      })}
+      </ScrollView>
     </NativeBaseProvider>
   );
 };
